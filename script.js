b1b0b78ba8511bda626a32c786d11c9156669e5f
Akash M <108210779+akashikur@users.noreply.github.com>
2023-07-02 19:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function volume_sphere() {
-    //Write your code here
-	V = 4/3 π r³
-	const volume =4/3 * 22/7 * r *r* r;
-	return volume
-  
+
+  var radius = parseFloat(document.getElementById("radius").value);
+
+  var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+  document.getElementById("volume").value = volume.toFixed(2);
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
